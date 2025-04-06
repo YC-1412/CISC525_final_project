353b8e09d57b6029fdb4cb2ffc1b99f4e057b162
yc <yingyucao.yc@gmail.com>
2025-04-06 00:25:33 -0400

    clean sh

--- scripts/process_202301.sh
+++ scripts/process_202301.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Process US COVID and flight data
-python src/process_data.py --year_month 2020 --country US
+python src/process_data.py --year_month 202301 --country US
 
 # # Navigate to the directory containing your script
 # cd scripts
 
 # # Make the script executable
-# chmod +x process_2020_US.sh
+# chmod +x process_202301_US.sh
 
 # # Or if you're in the project root directory:
-# chmod +x scripts/process_2020_US.sh
+# chmod +x scripts/process_202301_US.sh
